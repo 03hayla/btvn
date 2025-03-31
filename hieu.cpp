33b841a8f87c0d3150744b5036b149efd3fa4a19
03hayla <phamduchieudgnl@gmail.com>
2025-03-31 14:33:15 +0700

    Update hieu.cpp

--- hieu.cpp
+++ hieu.cpp
@@ -21,7 +21,7 @@ bài 3
 #include<bits/stdc++.h>
 using namespace std;
 void BubbleSort(int a[], int n){
-    int temp; // biến tạm temp
+    int temp; 
     for (int i = 0; i < n; i++){
 	for (int j = i + 1; j < n; j++){
 		if (a[j] > a[j+1]){
